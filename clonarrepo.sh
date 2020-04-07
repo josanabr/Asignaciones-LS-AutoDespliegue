e75e8178b2ecab9e1fe7b7074a8b0622c14fb53e
John Sanabria <john.sanabria@gmail.com>
2020-04-07 22:41:22 +0000

    se quito el segundo argumento

--- clonarrepo.sh
+++ clonarrepo.sh
@@ -2,22 +2,19 @@
 #
 # Este script clona un repositorio de GitHub en un directorio indicado.
 #
-# Este script espera recibir dos argumentos. En caso que no reciba dos argumentos
-# buscara las variables que requiere del archivo de configuracion 'config.cfg'.
+# Este script espera recibir un argumento. En caso que no reciba dicho argumento
+# buscara la variable 'GITURL' en el archivo de configuracion 'config.cfg'.
 #
-# En caso de recibir argumentos, El primer argumento es el link a GitHub para 
+# En caso de recibir el argumento, este argumento representa el link a GitHub para 
 # descargar los archivos del repositorio. Estos archivos son los encargados de 
 # hacer el monitoreo del servidor. 
-#
-# El segundo argumento es el directorio donde se hara el clonado del repositorio.
 #
 # AUTHOR: John Sanabria - john.sanabria@correounivalle.edu.co
 # DATE: 2020-04-07
 #
-if [ ${#} -ne 2 ]; then
+if [ ${#} -ne 1 ]; then
   . config.cfg
 else # pasaron dos argumentos al script
   GITURL="${1}"
-  DEPLOYDIR="${2}"
 fi
-git clone "${GITURL}" "${DEPLOYDIR}"
+git clone "${GITURL}" 
